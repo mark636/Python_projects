43170b8ca70e84df3d39388ca836e7b0c507d23a
mark636 <80371623+mark636@users.noreply.github.com>
2025-02-26 23:00:57 +1100

    Update SMS_service.py
    removed secrets

--- SMS_service.py
+++ SMS_service.py
@@ -1,13 +1,1 @@
 import requests
-from twilio.rest import Client
-
-account_sid = 'AC253934cc1acc7abc5b22adc6fbea1eff'
-auth_token = 'b59a266c0c80a5e9405b8eb314b8ea83'
-client = Client(account_sid, auth_token)
-message = client.messages.create(
-  messaging_service_sid='MGc41f0341ba72658184e36ef50be77457',
-  body='TEST MESSAGE FROM PYTHON',
-  from_='+18159498288',
-  to='+61420226498'
-)
-print(message.status)
